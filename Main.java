fd5ee33ac324c4e471c37f2d1c1eccf1f081ab97
henriquecdb <henriquecamposj@gmail.com>
2022-12-11 15:44:02 -0300

    Add basic stuff

--- Main.java
+++ Main.java
@@ -1,33 +1,64 @@
+import java.text.ParseException;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Scanner menu = new Scanner(System.in);
-
         while (true) {
-
-            System.out.print("##--Teste Estrutura de Menu--##\n\n");
+            System.out.print("##-----------Menu------------##\n");
             System.out.print("|-----------------------------|\n");
-            System.out.print("| Opção 1 - Nova Reserva     |\n");
-            System.out.print("| Opção 2 - Hospedes          |\n");
-            System.out.print("| Opção 3 - Produtos          |\n");
-            System.out.print("| Opção 4 - Sair              |\n");
+            System.out.print("| Opção 1 - Nova Reserva      |\n");
+            System.out.print("| Opção 2 - Listar Hospedes   |\n");
+            System.out.print("| Opção 3 - Registrar Hospede |\n");
+            System.out.print("| Opção 4 - Listar Quartos    |\n");
+            System.out.print("| Opção 5 - Sair              |\n");
             System.out.print("|-----------------------------|\n");
             System.out.print("Digite uma opção: ");
 
             int option = menu.nextInt();
 
-            if (option == 4) {
-                System.out.print("\nAté logo!");
-                menu.close();
+            if (option == 5) {
+                System.out.print("\nAté logo!\n");
                 break;
             }
 
             switch (option) {
-                case 1 -> System.out.print("\nOpção Novo Cadastro Selecionado");
-                case 2 -> System.out.print("\nOpção Clientes Selecionado\n");
-                case 3 -> System.out.print("\nOpção Produtos Selecionado\n");
-                default -> System.out.print("\nOpção Inválida!");
+                case 1 -> {
+                    System.out.print("\nOpção 'Nova Reserva' Selecionado\n");
+                    Scanner input = new Scanner(System.in);
+                    System.out.println("Digite a quantidade de acompanhantes: ");
+                    int guests = input.nextInt();
+                    System.out.println("Digite o ano desejado: ");
+                    int ano = input.nextInt();
+                    System.out.println("Digite o mes desejado: ");
+                    int mes = input.nextInt();
+                    System.out.println("Digite o dia desejado: ");
+                    int dia = input.nextInt();
+                    Date d = new Date(ano, mes, dia);
+                    if (Quarto.isAvailable(d)) {
+                        Reserva.makeReserve(Quarto.createRoom(guests), d, guests);
+                        Quarto.getVacancy().add(d);
+                        System.out.println("Reserva criada!\n");
+                    } else {
+                        System.out.println("Indisponivel!\n");
+                    }
+                }
+                case 2 -> {
+                    System.out.print("\nOpção 'Listar Hospedes' Selecionado\n");
+                    Hospede.listGuests();
+                }
+                case 3 -> {
+                    System.out.print("\nOpção 'Registrar Hospede' Selecionado\n");
+                    Scanner input = new Scanner(System.in);
+                    System.out.print("Entre com o Nome: \n");
+                    String name = input.next();
+                    System.out.print("Entre com o CPF: \n");
+                    String cpf = input.next();
+                    Hospede.registerGuest(name, cpf);
+                }
+                case 4 -> Quarto.listRooms();
+                default -> System.out.print("\nOpção Invalida!\n");
             }
         }
     }
