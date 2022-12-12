4e10a954936c30c6a38318a0f20f6fe44e770693
henriquecdb <henriquecamposj@gmail.com>
2022-12-12 17:20:25 -0300

    Another final main

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Quarto.createRooms();
         Scanner menu = new Scanner(System.in);
         while (true) {
             System.out.print("##------------Menu-------------##\n");
