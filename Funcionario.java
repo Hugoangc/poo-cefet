5fd69a4f94646fab7e0d3e433602ccae424a84f0
Hugoangc <81435251+Hugoangc@users.noreply.github.com>
2022-12-12 16:53:51 -0300

    Add files via upload

--- Funcionario.java
+++ Funcionario.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package poo_final;
 
-/**
- *
- * @author hugo1
- */
-public class Funcionario {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Funcionario extends Pessoa {
+    private String keyword;
+    private boolean isManager;
+    private double salario;
+    private static final ArrayList<Funcionario> login = new ArrayList<>();
+
+    public Funcionario(String nome, String CPF, String keyword){
+        super(nome, CPF);
+        this.salario = 7000;
+        this.keyword = keyword;
+        isManager = true;
+    }
+
+    public Funcionario(String nome, String CPF) {
+        super(nome, CPF);
+        this.salario = 4000;
+    }
+    public static Funcionario administrador(){
+        return new Funcionario("admin", "64277075070", "CefetPOO2");
+    }
+    public static boolean checkKeyword(String keyword, String keyword2){
+        return keyword.equals(keyword2);
+    }
+    public boolean promote(Funcionario f) {
+        if (!getIsManager()) return false;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite a nova senha para o funcionario: ");
+        String keyword = input.nextLine();
+        this.setKeyword(keyword);
+        //f.setIsManager(true);
+        login.add(f);
+        return true;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
     
+    public boolean setSalario(double salario) {
+        if(isManager) {
+            this.salario = salario;
+            return true;
+        }
+        else return false;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+    public boolean getIsManager() {
+        return isManager;
+    }
+    public void setIsManager(boolean isManager) {
+        this.isManager = isManager;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,67 +1,13 @@
-import java.util.Scanner;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo_final;
 
+/**
+ *
+ * @author hugo1
+ */
 public class Main {
-    public static void main(String[] args) {
-        Quarto.createRooms();
-        Scanner menu = new Scanner(System.in);
-        while (true) {
-            System.out.print("##-----------Menu------------##\n");
-            System.out.print("|-----------------------------|\n");
-            System.out.print("| Opção 1 - Nova Reserva      |\n");
-            System.out.print("| Opção 2 - Listar Hospedes   |\n");
-            System.out.print("| Opção 3 - Registrar Hospede |\n");
-            System.out.print("| Opção 4 - Listar Quartos    |\n");
-            System.out.print("| Opção 5 - Sair              |\n");
-            System.out.print("|-----------------------------|\n");
-            System.out.print("Digite uma opção: ");
-
-            int option = menu.nextInt();
-
-            if (option == 5) {
-                System.out.print("\nAté logo!\n");
-                break;
-            }
-
-            switch (option) {
-                case 1 -> {
-                    System.out.print("\nOpção 'Nova Reserva' Selecionado\n");
-                    Scanner input = new Scanner(System.in);
-                    System.out.println("Digite a quantidade de acompanhantes: ");
-                    int guests = input.nextInt();
-                    input.nextLine();
-                    System.out.println("Digite o tempo de estadia: ");
-                    int days = input.nextInt();
-                    input.nextLine();
-                    Quarto q = Quarto.searchRooms(guests);
-                    if (q == null) { System.out.println("Indisponivel!\n"); }
-                    else {
-                        int value = days * 50 + 25 * guests;
-                        System.out.print("Valor total: ");
-                        System.out.println(value);
-                        Reserva.makeReserve(q,guests);
-                        System.out.println("Reserva efetuada");
-                    }
-                }
-                case 2 -> {
-                    System.out.print("\nOpção 'Listar Hospedes' Selecionado\n");
-                    Hospede.listGuests();
-                }
-                case 3 -> {
-                    System.out.print("\nOpção 'Registrar Hospede' Selecionado\n");
-                    Scanner input = new Scanner(System.in);
-                    System.out.print("Entre com o Nome: \n");
-                    String name = input.nextLine();
-                    System.out.print("Entre com o CPF: \n");
-                    String cpf = input.nextLine();
-                    while (!Pessoa.isCPF(cpf)) {
-                        System.out.print("Entre com um CPF válido: \n");
-                        cpf = input.next();
-                    }
-                    Hospede.registerGuest(name, cpf);
-                }
-                case 4 -> Quarto.listRooms();
-                default -> System.out.print("\nOpção Invalida!\n");
-            }
-        }
-    }
+    
 }
